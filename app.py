9b770a9380c71d2778c7b0156b852b71da283e11
Nurzat <nurje.love@mail.ru>
2021-07-19 11:27:28 +0600

    add adding

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
@@ -6,5 +7,15 @@ app = Flask(__name__)
 def homepage():
     f = open('goods.txt','r', encoding='utf-8')
     txt = f.readlines()
-
     return render_template('index.html',goods = txt)
+
+@app.route('/add/', methods=["POST"])
+def add():
+    good = request.form["good"]
+    f = open('goods.txt','a+', encoding='utf-8')
+    f.write(good + "\n")
+    f.close()
+    return """ 
+    <h1>"Инвентарь пополнен"</h1>
+    <a href='/'>Домой</a>
+    """
